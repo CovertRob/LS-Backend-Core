0490ab76f570191b0d9ca065cd4a986ed538f84f
Robert Feconda <robby.feconda365@gmail.com>
2024-05-17 14:55:28 -0700

    adding small program calculator

---
+++ lesson_2/calculator.py
@@ -0,0 +1,33 @@
+# This is the first small program in lesson 2 of launch school PY101
+# Command line calculator
+# Ask the user for the first number
+# Ask the user for the second number
+# Ask the user for an operations to perform
+# Perform the operation on the two numbers
+# Print the result to the terminal
+
+print('Welcome to Calculator!')
+
+# Ask the user for the first number.
+print("What's the first number?")
+number1 = input()
+
+#Ask user for the second number.
+print("What's the second number?")
+number2 = input()
+
+print(f'{number1} {number2}')
+
+print('''What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide''')
+operation = input()
+
+if operation == '1': # '1' represents addition
+    output = int(number1) + int(number2)
+elif operation == '2': # '1' represents subtraction
+    output = int(number1) - int(number2)
+elif operation == '3': # '3' represents multiply
+    output = int(number1) * int(number2)
+elif operation == '4': # '4' represents division
+    outpput = int(number1) / int(number2)
+
+print(f'The result is: {output}')
