33d479213df0cd48911bd983ebb37c0312db1092
Robert Feconda <robby.feconda365@gmail.com>
2024-05-28 17:53:02 -0700

    Implemented main and several edge cases.

--- PY101/lesson_2/loan_calc.py
+++ PY101/lesson_2/loan_calc.py
@@ -26,31 +26,37 @@ APR = 0.0
 
 LOAN_DURATION = 0.0
 
-#-----------PSEUDOCODE-----------------#
-
-# Function that handles getting the 3 required inputs from the user
-# Does value checking
-
-# Function that contains monthly payment formula and returns payment in dollar and cents format
-
-# main function
-
-# name function
-
 # Helper function to keep track of prompt lines.
 def prompt(message):
 
     print(f'==> {message}')
 
-# input() returns string. Using ,'s assigns more than one value. Check for TypeError.
+# input() returns string. Using ,'s assigns more than one value.
+# Check for TypeError and ValueError if empty.
 def check_value(value):
     try:
         float(value)
     except TypeError:
         return True
+    except ValueError:
+        return True
     return False
 
-# Gets from input and returns loan ammmount, APR, and loan duration
+# Used to check if user wants to do another calculation.
+def check_continue(message):
+
+    another_calc = message.lower()
+    while another_calc not in ['yes', 'no']:
+        prompt('Please enter either yes or no.')
+        another_calc = input().lower()
+    if another_calc == 'yes':
+        return True
+    if another_calc == 'no':
+        prompt('Exiting calculator.')
+        return False
+
+# Gets from input and returns loan ammmount, APR, and loan duration.
+# Returns not currently used but for future implementations.
 def user_inputs():
     global LOAN_AMMOUNT
     global APR
@@ -59,6 +65,9 @@ def user_inputs():
     LOAN_AMMOUNT = input()
     while check_value(LOAN_AMMOUNT):
         prompt('Please enter a correctly formatted loan amount (XXXXXX.XX):')
+        LOAN_AMMOUNT = input()
+    while LOAN_AMMOUNT == str(0):
+        prompt('Please enter a non-zero loan ammount')
         LOAN_AMMOUNT = input()
 
     prompt('APR (0.XX):')
@@ -72,26 +81,34 @@ def user_inputs():
     while check_value(LOAN_DURATION):
         prompt('Please enter a corrected formatted loan duration (X.X in months):')
         LOAN_DURATION = input()
+    while LOAN_DURATION == str(0):
+        prompt('Please enter a non-zero loan duration')
+        LOAN_DURATION = input()
 
     return LOAN_AMMOUNT, APR, LOAN_DURATION
 
-
+# Used to set up program with beginning user inputs.
 def initialize():
-    prompt('Welcome to loan calculator!')
     prompt('Please enter the following values.')
     user_inputs() # sets globals and returns loan ammount, APR, loan duration
 
+# Calculates the monthly loan payment.
 def monthly_payment():
-    global LOAN_AMMOUNT
-    global LOAN_DURATION
-    global APR
-
-    return float(LOAN_AMMOUNT) * ((float(APR) / 12) / (1 - (1 + (float(APR) / 12)) ** (-float(LOAN_DURATION))))
+    if APR != '0':
+        return float(LOAN_AMMOUNT) * ((float(APR) / 12) / (1 - (1 + (float(APR) / 12)) ** (-float(LOAN_DURATION))))
+    return float(LOAN_AMMOUNT) / float(LOAN_DURATION)
 
 # Execute program.
 def main():
-    initialize()
-    prompt(f'Your monthly payment is ${monthly_payment():.2f}')
+    prompt('Welcome to loan calculator!')
+    while True:
+        initialize()
+        prompt(f'Your monthly payment is ${monthly_payment():.2f}')
+        prompt('Would you like to perform another calculation? Please enter yes or no.')
+        if check_continue(input()) is True:
+            prompt('Continuing with next calculation')
+        else:
+            break
 
 # Execution control function.
 if __name__ == "__main__":
