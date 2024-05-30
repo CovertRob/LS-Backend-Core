ba155305c171e81da9d3410c75a36c08e11dd56e
Robert Feconda <robby.feconda365@gmail.com>
2024-05-30 16:34:00 -0700

    Updating practice_problems

---
+++ PY101/lesson_3/practice_problems.py
@@ -0,0 +1,21 @@
+# How can you determine whether a given string ends 
+# with an exclamation mark (!)? Write some code that prints True or False 
+# depending on whether the string ends with an exclamation mark.
+
+# def ends_with_exclamation(word):
+#     return word.endswith('!')
+
+# print(ends_with_exclamation('Hello!'))
+
+# write two ways to prepend famous words to the front of a string
+
+famous_words = "seven years ago..."
+
+test = "are cool"
+
+def prepend_one(word, famous):
+    return famous + word
+
+# or
+
+print("{} {}".format(famous_words, test))
