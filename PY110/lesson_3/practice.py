c2bf46d6daef94ef0823b8dd6f288b2d4cd12f61
Robert Feconda <robby.feconda365@gmail.com>
2024-07-24 15:25:24 -0700

    Update practice.

--- PY110/lesson_3/practice.py
+++ PY110/lesson_3/practice.py
@@ -3115,11 +3115,44 @@ def is_balanced(s):
             return False
     return parens == 0
 
-print(is_balanced("What (is) this?") == True)        # True
-print(is_balanced("What is) this?") == False)        # True
-print(is_balanced("What (is this?") == False)        # True
-print(is_balanced("((What) (is this))?") == True)    # True
-print(is_balanced("((What)) (is this))?") == False)  # True
-print(is_balanced("Hey!") == True)                   # True
-print(is_balanced(")Hey!(") == False)                # True
-print(is_balanced("What ((is))) up(") == False)      # True
+# print(is_balanced("What (is) this?") == True)        # True
+# print(is_balanced("What is) this?") == False)        # True
+# print(is_balanced("What (is this?") == False)        # True
+# print(is_balanced("((What) (is this))?") == True)    # True
+# print(is_balanced("((What)) (is this))?") == False)  # True
+# print(is_balanced("Hey!") == True)                   # True
+# print(is_balanced(")Hey!(") == False)                # True
+# print(is_balanced("What ((is))) up(") == False)      # True
+
+'''
+
+Write a function that takes a floating point number representing an angle between 0 and 360 degrees and returns a string representing that angle in degrees, minutes, and seconds. You should use a degree symbol (°) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. There are 60 minutes in a degree, and 60 seconds in a minute.
+
+Note: You can use the following constant to represent the degree symbol:
+
+Problem: 
+    Need to turn xx.xxx... float into xx degrees xx minutes xx seconds
+    There are 60 minutes in a degree
+    There are 60 seconds in a minute
+    So it follows - every 1 integer is 60 minutes, 
+    What is the equation to convert degrees to dms?
+        sub-problem: figure out how the decimals conver to minutes and seconds
+            multiple remaing decimal by 60 to get minutes and then the remaining decimals again by 60 to get seconds
+
+'''
+
+def dms(input):
+    degrees = int(input//1)
+    minutes = (input - degrees) * 60
+    seconds = (minutes - (minutes//1)) * 60
+    print(f"{degrees}{DEGREE}{int(minutes)}'{int(seconds)}\"")
+
+DEGREE = "\u00B0"
+
+# All of these examples should print True
+print(dms(30) == "30°00'00\"")
+print(dms(76.73) == "76°43'48\"")
+print(dms(254.6) == "254°35'59\"")
+print(dms(93.034773) == "93°02'05\"")
+print(dms(0) == "0°00'00\"")
+print(dms(360) == "360°00'00\"" or dms(360) == "0°00'00\"")
