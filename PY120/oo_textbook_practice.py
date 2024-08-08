fafe9dc6b186479193c1760c27ca8ae0c3a606ea
Robert Feconda <robby.feconda365@gmail.com>
2024-08-07 19:04:11 -0700

    Update practice.

--- PY120/oo_textbook_practice.py
+++ PY120/oo_textbook_practice.py
@@ -69,7 +69,7 @@ class GoodDog:
 
         self._age = age
 
-sparky = GoodDog('Sparky', 5)
+# sparky = GoodDog('Sparky', 5)
 # print(sparky.name)          # Sparky
 # print(sparky.age)           # 5
 # sparky.name = 'Fireplug'
@@ -85,3 +85,78 @@ sparky = GoodDog('Sparky', 5)
 # sparky.name = 42  # TypeError: Name must be a string
 
 # sparky.age = -1   # ValueError: Age can't be negative
+
+# Create a Car class with the listed requirements
+
+class Car:
+
+    def __init__(self, model, year, color):
+        self._model = model
+        self._year = year
+        self._color = color
+        self.speed = 0
+    
+    def engine_start(self):
+        print('The engine is on!')
+
+    def engine_off(self):
+        self.speed = 0
+        print("Let's park this baby")
+        print("The engine is off")
+
+    def speed_up(self, number):
+        self.speed += number
+
+    def brake(self, number):
+        self.speed -= number
+    
+    def get_speed(self):
+        print(f'Your speed is {self.speed} mph')
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, paint):
+        self._color = paint
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def year(self):
+        return self._year
+
+lumina = Car('chevy lumina', 1997, 'white')
+# lumina.engine_start() # The engine is on!
+# lumina.get_speed()    # Your speed is 0 mph.
+# lumina.speed_up(20)   # You accelerated 20 mph.
+# lumina.get_speed()    # Your speed is 20 mph.
+# lumina.speed_up(30)   # You accelerated 30 mph.
+# lumina.get_speed()    # Your speed is 50 mph.
+# lumina.brake(15)      # You decelerated 15 mph.
+# lumina.get_speed()    # Your speed is 35 mph.
+# lumina.brake(30)      # You decelerated 30 mph.
+# lumina.get_speed()    # Your speed is 5 mph.
+# lumina.engine_off()   # Let's park this baby!
+#                       # The engine is off
+# lumina.get_speed()    # Your speed is 0 mph.
+    
+print(f'My car is {lumina.color}.')
+# My car is white.
+
+print(f"My car's model is a {lumina.model}.")
+# My car's model is a chevy lumina.
+
+print(f"My car's year is {lumina.year}.")
+# My car's year is 1997.
+
+lumina.color = 'brown'
+print(f'My car is now {lumina.color}.')
+# My car is now brown.
+
+lumina.year = 2023
+# AttributeError: property 'year' of 'Car' object
+# has no setter
