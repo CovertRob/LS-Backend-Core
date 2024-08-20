77ea3e5ae0f699328263a0e1951f7d76e13f13e4
Robert Feconda <robby.feconda365@gmail.com>
2024-08-19 22:13:40 -0700

    Update problem set.

--- PY120/problem_sets.py
+++ PY120/problem_sets.py
@@ -323,3 +323,43 @@ class SmartLamp:
                 return
 
         raise ValueError('Brightness must be between 0 and 100.')
+
+# Problem Set: Scope
+
+# Define a Dog class that has a breed instance variable. Instantiate two objects from this class, one with the breed 'Golden Retriever' and another with the breed 'Poodle'. Print the breed of each dog.
+
+class Dog:
+    def __init__(self, breed):
+        self.breed = breed
+
+dog1 = Dog('Golden Retriever')
+dog2 = Dog('Poodle')
+
+print(dog1.breed)  # Golden Retriever
+print(dog2.breed)  # Poodle
+
+# 2 Create a getter and mark for internal use
+class Dog:
+    def __init__(self, breed):
+        self._breed = breed
+
+    def get_breed(self):
+        return self._breed
+
+dog1 = Dog('Golden Retriever')
+dog2 = Dog('Poodle')
+
+print(dog1.get_breed())  # Golden Retriever
+print(dog2.get_breed())  # Poodle
+
+# 3. Create a Cat class that has a single method named get_name that returns the name instance variable. Without initializing name, try to instantiate a Cat object and call get_name. Print Name not set! when the error occurs.
+
+class Cat:
+    def get_name(self):
+        try:
+            return self.name
+        except AttributeError:
+            return "Name not set!"
+
+cat = Cat()
+print(cat.get_name())  # Name not set!
