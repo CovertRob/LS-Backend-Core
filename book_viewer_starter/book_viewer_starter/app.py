93ce4f22f38daee26f2251da64d09e994ab04119
CovertRob <robby.feconda365@gmail.com>
2024-10-29 00:06:42 -0700

    Update lesson 3.

--- book_viewer_starter/book_viewer_starter/app.py
+++ book_viewer_starter/book_viewer_starter/app.py
@@ -4,8 +4,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+
+    with open("book_viewer/data/toc.txt", "r") as file:
+        contents = file.readlines()
     
-    return render_template('index.html')
+    return render_template('index.html', contents=contents)
+
+@app.route("/chapters/1")
+def chp1():
+
+    title = "Chapter 1"
+    with open("book_viewer/data/toc.txt", "r") as file:
+        contents = file.readlines()
+    with open("book_viewer/data/chp1.txt", "r") as file:
+        chapter = file.read()
+
+    return render_template('chapter.html', title=title, contents=contents, chapter=chapter)
+    
+    
     
 
 if __name__ == "__main__":
