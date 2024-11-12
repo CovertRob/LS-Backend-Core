cb0a3f448173d840179024e1d9d50f2e2371226a
CovertRob <robby.feconda365@gmail.com>
2024-11-11 16:33:20 -0800

    Config signin.

--- PY175/cms/app.py
+++ PY175/cms/app.py
@@ -1,15 +1,41 @@
 from flask import Flask, render_template, send_from_directory, flash, redirect, url_for, request, session
 import os
 from markdown import markdown
+from functools import wraps
+import yaml
 
 app = Flask(__name__)
 app.secret_key = 'secret'
+
+def load_user_credentials():
+    filename = 'users.yaml'
+    root_dir = os.path.dirname(__file__)
+    if app.config['TESTING']:
+        credentials_path = os.path.join(root_dir, 'tests', filename)
+    else:
+        credentials_path = os.path.join(root_dir, "cms", filename)
+
+    with open(credentials_path, 'r') as file:
+        return yaml.safe_load(file)
 
 def get_data_path():
     if app.config['TESTING']:
         return os.path.join(os.path.dirname(__file__), 'tests', 'data')
     else:
         return os.path.join(os.path.dirname(__file__), 'cms/data')
+
+def require_login(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if user_signed_in():
+            return func(*args, **kwargs)
+        else:
+            flash("You must be signed in to do that.")
+            return redirect(url_for('index'))
+    return wrapper
+
+def user_signed_in():
+    return bool('username' in session)
 
 @app.route("/")
 def index():
@@ -35,6 +61,7 @@ def get_file(file_name):
         return redirect(url_for('index'))
     
 @app.route("/<file_name>/edit", methods=["GET"])
+@require_login
 def edit_file(file_name):
     data_dir = get_data_path()
     with open(data_dir + f'/{file_name}', 'r') as file:
@@ -42,6 +69,7 @@ def edit_file(file_name):
     return render_template('edit_file.html', content=contents, file_name=file_name, title=file_name)
 
 @app.route("/<file_name>/edit", methods=["POST"])
+@require_login
 def save_changes(file_name):
     data_dir = get_data_path()
     with open(data_dir + f'/{file_name}', 'w') as file:
@@ -51,11 +79,14 @@ def save_changes(file_name):
         session.modified = True
     return redirect(url_for('index'))
 
+
 @app.route("/new", methods=["GET"])
+@require_login
 def enter_new_document():
     return render_template('new_file.html')
 
 @app.route("/new", methods=["POST"])
+@require_login
 def save_new_document():
     new_doc = request.form['new_file']
     data_dir = get_data_path()
@@ -73,6 +104,45 @@ def save_new_document():
         flash(f"{new_doc} has been created.")
         session.modified = True
         return redirect(url_for('index'))
+    
+@app.route("/<filename>/delete", methods=['POST'])
+@require_login
+def delete_file(filename):
+    data_dir = get_data_path()
+    file_path = os.path.join(data_dir, filename)
+
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+        flash(f"{filename} has been deleted.")
+    else:
+        flash(f"{filename} does not exist.")
+
+    return redirect(url_for('index'))
+
+@app.route("/users/signin", methods=["GET"])
+def show_signin_form():
+    return render_template('signin.html')
+
+@app.route("/users/signin", methods=["POST"])
+def signin():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    credentials = load_user_credentials()
+    
+    if username in credentials and credentials[username] == password:
+        session['username'] = username
+        flash("Welcome!")
+        return redirect(url_for('index'))
+    else:
+        flash("Invalid credentials")
+        return render_template('signin.html'), 422
+    
+@app.route("/users/signout", methods=["POST"])
+def signout():
+    session.pop('username', None)
+    flash("You have been signed out.")
+    return redirect(url_for('index'))
         
 
 if __name__ == '__main__':
--- PY175/cms/tests/test_app.py
+++ PY175/cms/tests/test_app.py
@@ -65,42 +65,116 @@ class AppTest(unittest.TestCase):
 
     def test_edit_file(self):
         self.create_document("changes.txt")
-        response = self.client.get('/changes.txt/edit')
+        client = self.admin_session()
+        response = client.get('/changes.txt/edit')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content_type, "text/html; charset=utf-8")
         self.assertIn("<textarea", response.get_data(as_text=True))
 
     def test_updating_file(self):
         self.create_document("changes.txt")
-        response = self.client.post('/changes.txt/edit', data={'content': "new content"})
+        client = self.admin_session()
+        response = client.post('/changes.txt/edit', data={'content': "new content"})
         self.assertEqual(response.status_code, 302)
 
-        follow_response = self.client.get(response.headers['Location'])
+        follow_response = client.get(response.headers['Location'])
         self.assertIn("changes.txt has been updated.", follow_response.get_data(as_text=True))
 
-        with self.client.get("/changes.txt") as content_response:
+        with client.get("/changes.txt") as content_response:
             self.assertEqual(content_response.status_code, 200)
             self.assertIn("new content", content_response.get_data(as_text=True))      
 
     def test_enter_new_document(self):
-        response = self.client.get('/new')
+        client = self.admin_session()
+        response = client.get('/new')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content_type, "text/html; charset=utf-8")
         self.assertIn("<form action=\"/new\" method=\"post\">", response.get_data(as_text=True))
 
     def test_save_new_document(self):
-        response = self.client.post("/new", data={'new_file': "new_file.txt"})
+        client = self.admin_session()
+        response = client.post("/new", data={'new_file': "new_file.txt"})
         self.assertEqual(response.status_code, 302)
-        follow_response = self.client.get(response.headers['Location'])
+        follow_response = client.get(response.headers['Location'])
         self.assertIn("new_file.txt has been created.", follow_response.get_data(as_text=True))
 
-        with self.client.get("/new_file.txt") as content_response:
+        with client.get("/new_file.txt") as content_response:
             self.assertEqual(content_response.status_code, 200)
 
     def test_create_new_document_without_filename(self):
-        response = self.client.post('/new', data={'new_file': ''})
+        client = self.admin_session()
+        response = client.post('/new', data={'new_file': ''})
         self.assertEqual(response.status_code, 422)
         self.assertIn("A name is required.", response.get_data(as_text=True))
+
+    def test_deleting_document(self):
+        self.create_document("test.txt")
+        client = self.admin_session()
+        response = client.post('/test.txt/delete', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("test.txt has been deleted",
+                      response.get_data(as_text=True))
+
+        response = client.get('/')
+        self.assertNotIn("test.txt", response.get_data(as_text=True))
+
+    def test_signin_form(self):
+        client = self.admin_session()
+        response = client.get('/users/signin')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("<input", response.get_data(as_text=True))
+        self.assertIn('<button type="submit"', response.get_data(as_text=True))
+
+    def test_signin(self):
+        response = self.client.post('/users/signin',
+                                    data={
+                                        'username': 'admin',
+                                        'password': 'secret',
+                                    },
+                                    follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Welcome", response.get_data(as_text=True))
+        self.assertIn("Signed in as admin", response.get_data(as_text=True))
+
+    def test_signin_with_bad_credentials(self):
+        response = self.client.post('/users/signin',
+                                    data={
+                                        'username': 'guest',
+                                        'password': 'shhhh',
+                                    })
+        self.assertEqual(response.status_code, 422)
+        self.assertIn("Invalid credentials", response.get_data(as_text=True))
+
+    def test_signout(self):
+        self.client.post('/users/signin',
+                         data={'username': 'admin', 'password': 'secret'},
+                         follow_redirects=True)
+        response = self.client.post('/users/signout', follow_redirects=True)
+        self.assertIn("You have been signed out",
+                      response.get_data(as_text=True))
+        self.assertIn("Sign In", response.get_data(as_text=True))
+
+    def admin_session(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['username'] = 'admin'
+            return c
+
+    def test_require_login_routes(self):
+        self.create_document("test.txt")
+        resonse = self.client.get("/test.txt/edit")
+        self.assertEqual(resonse.status_code, 302)
+        response = self.client.post("/test.txt/edit", data={'content':'new content'})
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get("/new")
+        self.assertEqual(response.status_code, 302)
+        response = self.client.post("/new", data={'new_file': 'new_file.txt'})
+        self.assertEqual(response.status_code, 302)
+        response = self.client.post("/test.txt/delete")
+        self.assertEqual(response.status_code, 302)
+        self.admin_session()
+        response = self.client.post("/test.txt/delete")
+        self.assertEqual(response.status_code, 302)
 
 if __name__ == '__main__':
     unittest.main()
